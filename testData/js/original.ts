3c0a73a8f8b46d86e483855c6c57718d13612cf4
Gareth Jones <jones258@gmail.com>
2019-06-02 09:26:10 +1200

    Add to the test data for JS

--- testData/js/original.ts
+++ testData/js/original.ts
@@ -1,5 +1,6 @@
+// @formatter:off
 describe('root', () => {
-  describe('when something happens', () => {
+  describe('single quotes', () => {
     it('is true 1', () => {
       expect(true).toBe(true);
     });
@@ -9,5 +10,64 @@ describe('root', () => {
     it('is true 3', () => {
       expect(true).toBe(true);
     });
+  });
+  describe("double quotes", () => {
+    it("is true 1", () => {
+      expect(true).toBe(true);
+    });
+    it("is true 2", () => {
+      expect(true).toBe(true);
+    });
+    it("is true 3", () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe(`simple template literal`, () => {
+    it(`is true 1`, () => {
+      expect(true).toBe(true);
+    });
+    it(`is true 2`, () => {
+      expect(true).toBe(true);
+    });
+    it(`is true 3`, () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe(`complex template ${'literal'}`, () => {
+    it(`is true ${'1'}`, () => {
+      expect(true).toBe(true);
+    });
+    it(`is true ${"2"}`, () => {
+      expect(true).toBe(true);
+    });
+    it(`is ${true} 3`, () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe('mixed strings', () => {
+    it('is true 1', () => {
+      expect(true).toBe(true);
+    });
+    it("is true 2", () => {
+      expect(true).toBe(true);
+    });
+    it(`is ${true} 3`, () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe('new lines', () => {
+    it('is true 1', () => {
+      expect(true).toBe(true);
+    });
+
+    it("is true 2", () => {
+      expect(true).toBe(true);
+    });
+
+
+    it(`is ${true} 3`, () => {
+      expect(true).toBe(true);
+    });
+
   });
 });
