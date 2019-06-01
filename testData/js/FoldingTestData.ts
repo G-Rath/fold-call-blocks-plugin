9d833d660a3eb33fec333798215991eb6cd5b1d1
Gareth Jones <jones258@gmail.com>
2019-06-02 09:26:10 +1200

    Add to the test data for JS

--- testData/js/FoldingTestData.ts
+++ testData/js/FoldingTestData.ts
@@ -1,6 +1,6 @@
 // @formatter:off
 describe('root', () => <fold text='{...}'>{
-  <fold text='when something happens'>describe('when something happens', () => <fold text='{...}'>{
+  <fold text='single quotes'>describe('single quotes', () => <fold text='{...}'>{
     <fold text='is true 1'>it('is true 1', () => <fold text='{...}'>{
       expect(true).toBe(true);
     }</fold>);</fold>
@@ -10,5 +10,64 @@ describe('root', () => <fold text='{...}'>{
     <fold text='is true 3'>it('is true 3', () => <fold text='{...}'>{
       expect(true).toBe(true);
     }</fold>);</fold>
+  }</fold>);</fold>
+  <fold text='double quotes'>describe("double quotes", () => <fold text='{...}'>{
+    <fold text='is true 1'>it("is true 1", () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true 2'>it("is true 2", () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true 3'>it("is true 3", () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+  }</fold>);</fold>
+  <fold text='simple template literal'>describe(`simple template literal`, () => <fold text='{...}'>{
+    <fold text='is true 1'>it(`is true 1`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true 2'>it(`is true 2`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true 3'>it(`is true 3`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+  }</fold>);</fold>
+  <fold text='complex template ${`literal`}'>describe(`complex template ${`literal`}`, () => <fold text='{...}'>{
+    <fold text='is true ${`1`}'>it(`is true ${`1`}`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true ${"2"}'>it(`is true ${"2"}`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is ${true} 3'>it(`is ${true} 3`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+  }</fold>);</fold>
+  <fold text='mixed strings'>describe('mixed strings', () => <fold text='{...}'>{
+    <fold text='is true 1'>it('is true 1', () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is true 2'>it("is true 2", () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+    <fold text='is ${true} 3'>it(`is ${true} 3`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+  }</fold>);</fold>
+  <fold text='new lines'>describe('new lines', () => <fold text='{...}'>{
+    <fold text='is true 1'>it('is true 1', () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);
+</fold>
+    <fold text='is true 2'>it("is true 2", () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);
+
+</fold>
+    <fold text='is ${true} 3'>it(`is ${true} 3`, () => <fold text='{...}'>{
+      expect(true).toBe(true);
+    }</fold>);</fold>
+
   }</fold>);</fold>
 }</fold>);
