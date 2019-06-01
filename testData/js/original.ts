eded27a12c3c15b6252632087aecf933ae0dbf94
Gareth Jones <jones258@gmail.com>
2019-06-01 21:13:04 +1200

    Create some test data

---
+++ testData/js/original.ts
@@ -0,0 +1,13 @@
+describe('root', () => {
+  describe('when something happens', () => {
+    it('is true 1', () => {
+      expect(true).toBe(true);
+    });
+    it('is true 2', () => {
+      expect(true).toBe(true);
+    });
+    it('is true 3', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
