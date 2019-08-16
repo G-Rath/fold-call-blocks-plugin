ccd03572eea134a739535239b0c1b6de8d36b2fa
Gareth Jones <jones258@gmail.com>
2019-08-16 12:37:56 +1200

    Create `BlockMatcher` class

---
+++ src/main/java/jones/foldtestblocks/config/BlockMatcher.java
@@ -0,0 +1,36 @@
+package jones.foldtestblocks.config;
+
+public class BlockMatcher {
+  private String blockIdentifier;
+  private boolean prefixWhenFolding;
+
+  @SuppressWarnings("unused")
+  public BlockMatcher() {
+    this("", true);
+  }
+
+  public BlockMatcher(BlockMatcher existingRule) {
+    this(existingRule.blockIdentifier, existingRule.prefixWhenFolding);
+  }
+
+  public BlockMatcher(String blockIdentifier, boolean prefixWhenFolding) {
+    this.blockIdentifier = blockIdentifier;
+    this.prefixWhenFolding = prefixWhenFolding;
+  }
+
+  public String getBlockIdentifier() {
+    return blockIdentifier;
+  }
+
+  public void setBlockIdentifier(String blockIdentifier) {
+    this.blockIdentifier = blockIdentifier;
+  }
+
+  public boolean isPrefixWhenFolding() {
+    return prefixWhenFolding;
+  }
+
+  public void setPrefixWhenFolding(boolean prefixWhenFolding) {
+    this.prefixWhenFolding = prefixWhenFolding;
+  }
+}
