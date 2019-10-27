6674a39bbceaeaae6f889f8cd30a1710d62b8976
Gareth Jones <jones258@gmail.com>
2019-10-27 23:30:04 +1300

    Add default block matchers to `BlockMatchersManager`

--- src/main/java/jones/foldcallblocks/BlockMatchersManager.java
+++ src/main/java/jones/foldcallblocks/BlockMatchersManager.java
@@ -9,12 +9,40 @@ import com.intellij.openapi.util.SimpleModificationTracker;
 import jones.foldcallblocks.config.BlockMatcher;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @State(name = "BlockMatcherManagerConfiguration", storages = @Storage("block-folding-matchers.xml"))
 public class BlockMatchersManager extends SimpleModificationTracker implements PersistentStateComponent<BlockMatchersManager> {
-  private List<BlockMatcher> blockMatchers = new ArrayList<>();
+  private List<BlockMatcher> blockMatchers = buildDefaultMatchers();
+
+  private static List<BlockMatcher> buildDefaultMatchers() {
+    return (
+      Stream.of(
+        Stream.of(
+          "describe",
+          "test",
+          "it"
+        ).flatMap(s -> Stream.of(
+          s,
+          s.concat(".skip"),
+          s.concat(".only")
+        )),
+        Stream.of(
+          "xdescribe",
+          "fdescribe",
+          "xtest",
+          "xit",
+          "fit"
+        )
+      )
+            .flatMap(o -> o)
+            .flatMap(s -> Stream.of(s, s.concat(".each")))
+            .map(block -> new BlockMatcher(block, !block.contains("describe")))
+            .collect(Collectors.toList())
+    );
+  }
 
   public static BlockMatchersManager getInstance(@NotNull Project project) {
     return ServiceManager.getService(project, BlockMatchersManager.class);
